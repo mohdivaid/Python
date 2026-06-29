165952cc6d190dcadd02dff1d3048bd2daaad458
mohdivaid <ivaidkhan111@gmail.com>
2026-06-29 19:05:23 +0530

    loops

---
+++ loops/loop4.py
@@ -0,0 +1,7 @@
+sum = 0
+for i in range(12):
+    
+    if(i/2 != 0):
+        sum+=i
+    
+print(sum)
---
+++ loops/loop5.py
@@ -0,0 +1,5 @@
+num = 8
+for i in range(11):
+    if(i<11):
+        total=num*i
+        print(total)
