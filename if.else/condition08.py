846f77a6f09f97672a0641e21e89e93b8cc10be4
MOHD IVAID <ivaidkhan111@gmail.com>
2026-07-03 03:04:02 +0530

    adding conditions

---
+++ if.else/condition08.py
@@ -0,0 +1,8 @@
+#Write a program to input angles of a triangle and check whether triangle is valid or not.
+angle1=60
+angle2=10
+angle3=60
+if(angle1+angle2+angle3==180):
+    print("The triangle is valid")
+else:
+    print("The triangle is not valid")
