272fa4b91be94bb20012e41397dc317df23bedd0
mohdivaid <ivaidkhan111@gmail.com>
2026-07-03 19:20:10 +0530

    update

--- if.else/condition07.py
+++ if.else/condition07.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #Write a program to count total number of notes in given amount.
 x=195
 if(x>=500): #for 500 notes
@@ -24,31 +24,4 @@ if(x>=20): #for 20 notes
 if(x>=10): #for 10 notes
     f=x//10
     x=x%10
-=======
-#Write a program to count total number of notes in given amount.
-x=195
-if(x>=500): #for 500 notes
-    a=x//500
-    x=x%500
-    print("Total number of 500 notes:",a)
-if(x>=200): #for 200 notes
-    b=x//200
-    x=x%200
-    print("Total number of 200 notes:",b)
-if(x>=100): #for 100 notes
-    c=x//100
-    x=x%100
-    print("Total number of 100 notes:",c)
-if(x>=50): #for 50 notes
-    d=x//50
-    x=x%50
-    print("Total number of 50 notes:",d)
-if(x>=20): #for 20 notes        
-    e=x//20
-    x=x%20
-    print("Total number of 20 notes:",e)
-if(x>=10): #for 10 notes
-    f=x//10
-    x=x%10
->>>>>>> 39320e25694dfecbe212c4a1734b808b173f916b
     print("Total number of 10 notes:",f)
