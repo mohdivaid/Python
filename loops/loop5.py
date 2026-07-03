6768ff8ad5e3d9c5f8cb4c17768ad6cdc130d9a4
mohdivaid <ivaidkhan111@gmail.com>
2026-07-04 04:19:04 +0530

    some changes

--- loops/loop5.py
+++ loops/loop5.py
@@ -1,6 +1,5 @@
 #Write a program to print multiplication table of any number.   
 x = 7
-for i in range(21):
-    if(i<11):
-        total=x*i
-        print(total)
+for i in range(1, 11):
+    total = x * i
+    print(total)
