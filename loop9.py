ebd5b267dfe59e7fda1c810e939d6585857d065c
mohdivaid <ivaidkhan111@gmail.com>
2026-07-08 02:53:10 +0530

    adding loops

---
+++ loop9.py
@@ -0,0 +1,12 @@
+# Write a program to find first and last digit of a number and sum of both.
+
+A = "56789"
+
+firstletter = A[0]
+lastletter = A[-1]
+
+sum = firstletter + lastletter
+
+print("Firstdigit", firstletter)
+print("Lastdigit", lastletter)
+print("Sum of first and last digit:", sum)
