adac1f1af5abefbe32ece88b30cf12f11b5be5ac
mohdivaid <ivaidkhan111@gmail.com>
2026-07-09 20:14:20 +0530

    updating loops

--- loops/loop14.py
+++ loops/loop14.py
@@ -1,6 +1,6 @@
 #Write a program to print all Prime numbers between 1 to n.
 n = 10
-for num in range(1, n + 1):
+for num in range(2, n + 1):
     prime = True
     
     for i in range(2, int(num // 2) + 1):
