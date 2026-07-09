0f50708aad3e1b14c7275a04393cf65327fef2b0
mohdivaid <ivaidkhan111@gmail.com>
2026-07-09 20:15:01 +0530

    update

--- loops/loop13.py
+++ loops/loop13.py
@@ -1,8 +1,10 @@
 #Write a program to calculate factorial of a number.
 
-n = int(input("Enter a number: "))
+n = 3
 
 factorial = 1
 for i in range(1, n + 1):
+    
     factorial *= i
+    
 print("Factorial of n:", factorial)
--- loops/loop16.py
+++ loops/loop16.py
@@ -1,3 +1,14 @@
-#Write a program to check whether a number is Strong number or not.
+#Write a program to check whether a number is armStrong number or not
+a = 153
+sum = 0
+temp = a
 
-n = 421
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp=temp// 10
+
+if sum == a:
+    print("a is an Armstrong number.")
+else:
+    print("a is not an Armstrong number.")
