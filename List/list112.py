844ab4ce0a28e4bfb962782c013b0a26d4b8a535
mohdivaid <ivaidkhan111@gmail.com>
2026-07-14 19:57:58 +0530

    updaing all list elements

--- List/list112.py
+++ List/list112.py
@@ -1,2 +1,12 @@
 #Write a program to put even and odd elements of array in two separate arrays
 A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b=[]
+c=[]
+for i in A:
+    if i%2==0:
+        b.append(i)
+    else:
+        c.append(i)
+        
+print("Even elements in the array:", b)
+print("Odd elements in the array:", c)
