597bf98aef9a8eefe68b4743b9e39ea65439bd6c
mohdivaid <ivaidkhan111@gmail.com>
2026-07-15 19:28:43 +0530

    updating list

--- List/list10.py
+++ List/list10.py
@@ -1,12 +1,13 @@
 #Write a program to count total number of duplicate elements in an array.
-A = [1, 2, 3, 4, 1, 5, 4, 1]
+a = [1, 2, 3, 4, 1, 5, 4, 1]
 
 b = []            
 
 for i in range (len(a)):
+    
     for j in range(i + 1, len(a)):  
         if a[i] == a[j] and a[i] not in b:       
                 
-                b.append(a[i])
+         b.append(a[i])
 
 print("Duplicate elements:", b)
