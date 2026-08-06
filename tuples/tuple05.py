5f23e0f6330aa03f3a4bacffb178d6b6f62f8c58
mohdivaid <ivaidkhan111@gmail.com>
2026-08-06 19:14:30 +0530

    adding tuple

--- tuples/tuple05.py
+++ tuples/tuple05.py
@@ -1,9 +1,31 @@
-n = int(input("Enter the number of elements in the tuple: "))
-print("total number of elements:", n)
-if n % 2 == 0:
-    print("The number of elements is even")
-else:
-    print("The number of elements is odd")
-print("maximum element:", max(n))
-print("minimum element:", min(n))
-print("sum of elements:", sum(n))
+n = int(input("How many numbers: "))
+numbers = []
+for i in range(n):
+    num = int(input())
+    numbers.append(num)
+    t= tuple(numbers)
+print(t)
+print("total number:",len(t))
+largest=t[0]
+smallest=t[0]
+for i in t:
+    if (i>=largest):
+        largest=i
+    if(i<=smallest):
+        smallest=i
+print("largest number",largest)
+print("smallest number",smallest)
+count=0
+for j in t:
+    count+=j
+print(f"total number: {count}")
+print(f"average number;{count/len(t)}")
+even=0
+odd=0
+for z in t:
+    if(z%2==0):
+        even+=1
+    else:
+        odd+=1
+print(f"count of even number {even}")
+print(f"count of odd number {odd}")
