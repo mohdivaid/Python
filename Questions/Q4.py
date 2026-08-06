f651594b320b126c9b7498d14826917166e31f0e
mohdivaid <ivaidkhan111@gmail.com>
2026-08-06 19:15:07 +0530

    solving question

---
+++ Questions/Q4.py
@@ -0,0 +1,3 @@
+a=[1,2,3,4,4,5,4,5,74,4]
+unique = list(set(a))
+print("unique elements:",unique)
---
+++ Questions/Q5.py
@@ -0,0 +1,5 @@
+a=[1,23,55,66,44,88]
+b=[]
+for i in range(len(a)-1,-1,-1):
+    b.append(a[i])
+print(b)
