acc3dcb16eb80220d5f05185411f7834d80e0b5b
mohdivaid <ivaidkhan111@gmail.com>
2026-08-07 23:03:45 +0530

    solving question

--- Questions/Q8.py
+++ Questions/Q8.py
@@ -1,11 +1,14 @@
-student = {"Rahul": 75, "Aman": 45, "Priya": 82, "Sneha": 58, "Vikas": 60}
-
+student = {
+    "Rahul": 75,
+    "Aman": 45,
+    "Priya": 82,
+    "Sneha": 58,
+    "Vikas": 60
+}
 highest_marks = 0
-name = ""
-
+name=""
 for student_name, marks in student.items():
     if marks > highest_marks:
         highest_marks = marks
         name = student_name
-
-print(name)  
+print(highest_marks,"=",name)
